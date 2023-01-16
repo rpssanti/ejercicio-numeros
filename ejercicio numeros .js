747b72b4dfa236a16034c7c6a6393228423708d4
Santi <121914283+rpssanti@users.noreply.github.com>
2023-01-16 11:00:05 +0100

    Add files via upload

---
+++ ejercicio numeros .js
@@ -0,0 +1,31 @@
+//Ejercicio numeros
+
+let altura = 179 
+
+console.log(179)
+console.log(typeof altura)
+
+let alturam = 1.79 
+
+console.log(1.79)
+console.log(typeof alturam)
+
+let peso = 89.4
+
+console.log(89.5)
+console.log(typeof peso)
+
+let altura2 = 1.79
+
+console.log(altura2.toFixed(1))
+
+let peso2 = 89.4
+
+console.log(peso2.toFixed(0))
+
+
+let max_valor_JS = Number.EPSILON
+console.log(max_valor_JS) 
+
+console.log(max_valor_JS + 1 )
+
